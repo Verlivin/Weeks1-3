86e28b080a849fd635d4498855ca0e2a33200585
Verlivin <dan1829647051@gmail.com>
2026-01-19 09:41:15 -0500

    change color and htibox

---
+++ Assets/script/Sq.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Sq : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    public Color col;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //PickARandomColour();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(Keyboard.current.anyKey.wasPressedThisFrame)
+        //{
+        //    PickARandomColour();
+        // }
+
+         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        if(spriteRenderer.bounds.Contains(mousePos) == true)
+        {
+            spriteRenderer.color = col; 
+        }
+        else
+        {
+            spriteRenderer.color = Color.white;
+        }
+    
+    }
+    void PickARandomColour()
+    {
+        spriteRenderer.color = Random.ColorHSV();
+    }
+}
